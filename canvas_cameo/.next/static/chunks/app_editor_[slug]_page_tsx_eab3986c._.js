497b789e560f261f881f7cc6c54b853e03acfe75
Jackster042 <feemail042@gmail.com>
2025-04-18 22:04:48 +0200

    Env Default setup done

--- canvas_cameo/.next/static/chunks/app_editor_[slug]_page_tsx_eab3986c._.js
+++ canvas_cameo/.next/static/chunks/app_editor_[slug]_page_tsx_eab3986c._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_2e044425._.js"
+  "static/chunks/_c5428bd4._.js"
 ],
     source: "dynamic"
 });

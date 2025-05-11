4e289fa8773dcf40a7ae66a88a307a4839cc9e4b
Nemanja Stojanovic <feemail042@gmail.com>
2025-05-11 23:07:21 +0200

    Buildig Frontend - Uplaod Panel completed => adding upload images to canvas

--- canvas_cameo/components/editor/sidebar/panels/upload.tsx
+++ canvas_cameo/components/editor/sidebar/panels/upload.tsx
@@ -12,9 +12,10 @@ import { useEditorStore } from "@/store";
 // NEXT AUTH
 import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { Upload } from "lucide-react";
+import { Loader2, Upload } from "lucide-react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { uploadFileWithAuth } from "@/services/uplaod-service";
 
 function UploadPanel() {
   const [isUploading, setIsUploading] = useState(false);
@@ -43,7 +44,28 @@ function UploadPanel() {
   }, [status, ST]);
 
   // HANDLE FILE UPLOAD
-  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.files, "files from handleFileUpload");
+    const file = e.target.files?.[0];
+
+    setIsUploading(true);
+
+    try {
+      if (!file) throw new Error("No file selected");
+
+      const result = await uploadFileWithAuth(file);
+      console.log(result, "result from handleFileUpload");
+
+      setUserUploads((prev) => [result?.data, ...prev]);
+    } catch (err) {
+      console.error(err, "Error from handleFileUpload");
+    } finally {
+      setIsUploading(false);
+      e.target.value = "";
+    }
+  };
+
+  console.log(userUploads, "userUploads from UploadPanel");
 
   // FETCH USER UPLOADS
   useEffect(() => {
@@ -54,10 +76,14 @@ function UploadPanel() {
 
   return (
     <div className="h-full overflow-y-auto">
-      <div className="p-4 space-x-auto">
+      <div className="p-4 space-y-4">
         <div className="flex gap-2 ">
-          <Label>
-            <Upload className="w-5 h-5 mr-2" />
+          <Label
+            className={`w-full h-12 px-4 py-3 gap-2 bg-purple-600 hover:bg-purple-700 text-white rounded-md flex items-center justify-center cursor-pointer transition-colors
+              ${isUploading ? "opacity-70 cursor-not-allowed" : ""}
+              `}
+          >
+            <Upload className="w-4 h-4 mr-2" />
             <span className="font-medium">
               {isUploading ? "Uploading..." : "Upload files"}
             </span>
@@ -70,18 +96,30 @@ function UploadPanel() {
             />
           </Label>
         </div>
-      </div>
-      <div className="pt-2">
-        <h4 className="text-lg font-medium mb-4 text-gray-800 ml-4">
-          Your Uploads
-        </h4>
-        <div className="space-x-4">
-          {userUploads && userUploads.length > 0 ? (
-            userUploads.map((upload) => (
-              <div key={upload.id}>{upload.name}</div>
-            ))
+        <div className="mt-5">
+          <h4 className="text-sm text-gray-500 mb-5">Your Uploads</h4>
+          {isLoading ? (
+            <div className="border p-6 flex items-center justify-center rounded-md">
+              <Loader2 className="w-4 h-4" />
+              <p className="font-bold text-sm">Loading you uploads ...</p>
+            </div>
+          ) : userUploads.length > 0 ? (
+            <div className="grid grid-cols-3 gap-4">
+              {userUploads.map((imageData) => (
+                <div
+                  key={imageData._id}
+                  className="aspect-auto bg-gray-50 rounded-md overflow-hidden hover:opacity-85 transition-opacity relative group"
+                >
+                  <img
+                    src={imageData.url}
+                    alt={imageData || "image"}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              ))}
+            </div>
           ) : (
-            <div>No uploads found</div>
+            ""
           )}
         </div>
       </div>

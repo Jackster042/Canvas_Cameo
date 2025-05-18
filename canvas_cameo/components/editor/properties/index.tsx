17ba19d58de050fcf80a657c11359e0c288be95d
Jackster042 <feemail042@gmail.com>
2025-05-18 21:26:27 +0200

    Building Frontend - Text Properties setup done | css fix left

--- canvas_cameo/components/editor/properties/index.tsx
+++ canvas_cameo/components/editor/properties/index.tsx
@@ -18,6 +18,16 @@ import {
   cloneSelectedObject,
   deleteSelectedObject,
 } from "@/fabric/fabric-utils";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
+import { SelectTrigger } from "@radix-ui/react-select";
+import { fontFamilies } from "@/config";
 
 function Properties() {
   const { canvas } = useEditorStore();
@@ -26,6 +36,7 @@ function Properties() {
   const [selectedObject, setSelectedObject] = useState<FabricObject | null>(
     null
   );
+  const [objectType, setObjectType] = useState<string>("");
 
   // TODO:  LIST OF PROPERTIES TO BE ADDED
 
@@ -53,7 +64,10 @@ function Properties() {
 
   // OPACITY
 
-  const updateObjectProperty = (property: string, value: number | boolean) => {
+  const updateObjectProperty = (
+    property: string,
+    value: number | boolean | string
+  ) => {
     // console.log(property, value, "property and value");
     if (!canvas || !selectedObject) return;
 
@@ -105,21 +119,46 @@ function Properties() {
   };
 
   // TEXT HANDLERS
-  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {};
   const handleFontSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
-  const handleFontFamilyChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
-  const handleToggleBold = () => {};
-  const handleToggleItalic = () => {};
-  const handleToggleUnderline = () => {};
+  const handleFontFamilyChange = (value: string) => {
+    setFontFamily(value);
+    updateObjectProperty("fontFamily", value);
+  };
+  const handleToggleBold = () => {
+    const newWeight = fontWeight === "bold" ? "normal" : "bold";
+    setFontWeight(newWeight);
+    updateObjectProperty("fontWeight", newWeight);
+  };
+  const handleToggleItalic = () => {
+    const newStyle = fontStyle === "italic" ? "normal" : "italic";
+    setFontStyle(newStyle);
+    updateObjectProperty("fontStyle", newStyle);
+  };
+  const handleToggleUnderline = () => {
+    const newUnderline = !underline;
+    setUnderline(newUnderline);
+    updateObjectProperty("underline", newUnderline);
+  };
   const handleToggleTextColorChange = (
     e: React.ChangeEvent<HTMLInputElement>
-  ) => {};
+  ) => {
+    const newTextColor = e.target.value;
+    setTextColor(newTextColor);
+    updateObjectProperty("fill", newTextColor);
+  };
   const handleToggleTextBackgroundColorChange = (
     e: React.ChangeEvent<HTMLInputElement>
-  ) => {};
-  const handleLetterSpacingChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {};
+  ) => {
+    const newTextBgColor = e.target.value;
+    setTextBackgroundColor(newTextBgColor);
+    updateObjectProperty("backgroundColor", newTextBgColor);
+  };
+  const handleLetterSpacingChange = (value: number[]) => {
+    const newLetterSpacing = value[0];
+    setLetterSpacing(newLetterSpacing);
+    updateObjectProperty("charSpacing", newLetterSpacing);
+  };
 
   useEffect(() => {
     if (!canvas) return;
@@ -127,12 +166,25 @@ function Properties() {
     const handleSelectionCreated = () => {
       const activeObject = canvas.getActiveObject();
       if (activeObject) {
+        // console.log(activeObject.type, "active object type");
         setSelectedObject(activeObject);
         // console.log(activeObject, "active object");
         // UPDATE COMMON PROPERTIES
         setOpacity(Math.round(activeObject?.opacity! * 100) || 100);
         setWidth(Math.round(activeObject.width * activeObject.scaleX));
         setHeight(Math.round(activeObject.height * activeObject.scaleY));
+
+        // CHECK BASED ON TYPE
+        if (activeObject.type === "i-text") {
+          setObjectType("text");
+          // console.log(activeObject.type, "object type");
+        } else if (activeObject.type === "image") {
+          setObjectType("image");
+        } else if (activeObject.type === "path") {
+          setObjectType("path");
+        } else {
+          setObjectType("shape");
+        }
       }
     };
     const handleSelectionCleared = () => {};
@@ -159,13 +211,13 @@ function Properties() {
   // console.log(selectedObject, "selected object");
 
   return (
-    <div className="fixed right-0 top-[56px] bottom-[0px] w-[280px] bg-white border border-gray-200 z-10">
+    <div className="fixed right-0 top-[56px] bottom-[0px] w-[280px] bg-white border-1 border-gray-200 z-10">
       <div className="flex items-center justify-between p-3 border-b">
         <div className="flex items-center gap-2">
           <span className="font-medium">Properties</span>
         </div>
       </div>
-      <div className="h-[calc(100% - 96px)] overflow-auto p-4 space-y-6">
+      <div className="h-[calc(100%-96px)] overflow-auto p-4 space-y-6">
         <h3 className="text-sm font-medium">Size & Position</h3>
         {/* WIDTH AND HEIGHT PROPERTY */}
         <div className="grid grid-cols-2 gap-3">
@@ -268,6 +320,145 @@ function Properties() {
             </Button>
           </div>
         </div>
+
+        {/* TEXT RELATED PROPERTIES */}
+        {objectType === "text" && (
+          <div className="space-y-4 border-t">
+            <h3 className="text-sm font-medium">Text Properties</h3>
+            {/* TEXT */}
+            <div className="space-y-2">
+              <Label className={"text-xs"} htmlFor="text-content">
+                Text Content
+              </Label>
+              <Textarea
+                id="text-content"
+                value={text}
+                onChange={handleTextChange}
+                className={"h-20 resize-none"}
+              />
+            </div>
+            {/* FONT SIZE */}
+            <div className="space-y-2">
+              <Label className={"text-xs"} htmlFor="font-size">
+                Font Size
+              </Label>
+              <Input
+                id="font-size"
+                value={fontSize}
+                onChange={(e) => handleFontSizeChange(e)}
+                className={"w-16 h-7 text-xs"}
+                type={"number"}
+              />
+            </div>
+            {/* FONT FAMILY */}
+            <div className="space-y-2">
+              <Label htmlFor="font-family" className="text-sm">
+                Font Family
+              </Label>
+              <Select value={fontFamily} onValueChange={handleFontFamilyChange}>
+                <SelectTrigger className={"h-10"} id="font-family">
+                  <SelectValue placeholder="Select a font" />
+                </SelectTrigger>
+                <SelectContent>
+                  {fontFamilies.map((fontItem) => (
+                    <SelectItem
+                      key={fontItem}
+                      value={fontItem}
+                      style={{ fontFamily: fontItem }}
+                    >
+                      {fontItem}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            {/* TOGGLE STYLE */}
+            <div className="space-y-2">
+              <Label className="text-sm">Style</Label>
+              <div className="flex gap-4">
+                <Button
+                  variant={fontWeight === "bold" ? "default" : "outline"}
+                  onClick={handleToggleBold}
+                  size="icon"
+                  className={"w-12 h-8"}
+                >
+                  Bold
+                </Button>
+                <Button
+                  variant={fontStyle === "italic" ? "default" : "outline"}
+                  onClick={handleToggleItalic}
+                  size="icon"
+                  className={"w-12 h-8 items-center"}
+                >
+                  Italic
+                </Button>
+                <Button
+                  variant={underline ? "default" : "outline"}
+                  onClick={handleToggleUnderline}
+                  size="icon"
+                  className={"w-12 h-8 overflow-hidden"}
+                >
+                  {"______"}
+                </Button>
+              </div>
+            </div>
+            <div className="flex justify-between">
+              {/* TEXT COLOR AND BG COLOR */}
+              <div className="space-y-2">
+                <Label htmlFor="text-color" className="text-sm">
+                  Text Color
+                </Label>
+                <div className="relative w-8 h-8 overflow-hidden rounded-md border">
+                  <div
+                    className="absolute inset-0"
+                    style={{ backgroundColor: textColor }}
+                  />
+                  <Input
+                    id="text-color"
+                    type="color"
+                    value={textColor}
+                    onChange={handleToggleTextColorChange}
+                    className="absolute inset-0 opacity-0 cursor-pointer"
+                  />
+                </div>
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="text-bg-color" className="text-sm">
+                  Text BG Color
+                </Label>
+                <div className="relative w-8 h-8 overflow-hidden rounded-md border">
+                  <div
+                    className="absolute inset-0"
+                    style={{ backgroundColor: textBackgroundColor }}
+                  />
+                  <Input
+                    id="text-bg-color"
+                    type="color"
+                    value={textBackgroundColor}
+                    onChange={handleToggleTextBackgroundColorChange}
+                    className="absolute inset-0 opacity-0 cursor-pointer"
+                  />
+                </div>
+              </div>
+            </div>
+            {/* LETTER SPACING */}
+            <div className="space-y-2">
+              <div className="flex justify-between">
+                <Label htmlFor="letter-spacing" className="text-sm">
+                  Letter Spacing
+                </Label>
+                <span className="text-xs">{letterSpacing}</span>
+                <Slider
+                  id="letter-spacing"
+                  min={-200}
+                  max={800}
+                  value={[letterSpacing]}
+                  onValueChange={(value) => handleLetterSpacingChange(value)}
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
